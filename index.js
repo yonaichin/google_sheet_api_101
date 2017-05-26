006d02154823933d54e0ce20cc025ea27931adfb
Olly.W <yonaichin@gmail.com>
2017-05-26 18:23:02 +0800

    generate query_matrix

--- index.js
+++ index.js
@@ -1,6 +1,87 @@
-const google      = require('googleapis');
-const credentials = require('./credentials.json');
+import google from 'googleapis'
+import _ from 'lodash'
+import mongodb from 'mongodb'
+import mongoose from 'mongoose'
+import colors from 'colors'
+import R from 'ramda'
 
+import credentials from './credentials.json'
+import DB from './configs/db.json'
+import Product from './models/product.js'
+
+const MONGODB_URI = process.env.MONGODB_URI || `mongodb://localhost/ls-endowment`
+let db
+
+mongoose.connect(MONGODB_URI)
+// Plugging in your own Promises Library using ES6 Promise
+// Ref: http://mongoosejs.com/docs/promises.html
+mongoose.Promise = global.Promise
+
+db = mongoose.connection
+
+db.on('error', (err) => {
+  console.error('MongoDB connection error: ', err)
+  process.exit(1)
+})
+db.on('open', () => {
+  console.log('db connected')
+  // let product = new Product()
+  // product.age = '26'
+  // product.gender = 'male'
+  // product.ins_amount = '5000000'
+  // product.ins_pay_period = '6'
+  // product.ins_fee = '6000'
+  // product.ins_fee_with_discount = '5555'
+  // product.forfeit_fee_matrix = 'table~~'
+  // product.save((err) => {
+  //   if (err) {
+  //     console.log('save error', err.message)
+  //   } else {
+  //   }
+  // })
+
+
+  // Product.find({age: '25'}, (err, p) => {
+  //   if (err) {
+  //     console.log('get error', err.message)
+  //   } else {
+  //     console.log('Product from db', JSON.stringify(p))
+  //   }
+
+  // })
+})
+const age_start = 30
+const age_end = 40
+const age_arr = _.range(age_start, age_end + 1, 1)
+const gender = ['male', 'female']
+const ins_amount_start = 5000
+const ins_amount_end = 10000
+const ins_amount_offset = 1000
+const ins_pay_period = [null, 3, 6, 10]
+const timer_interval = 0.5
+const ins_amount_arr = _.range(ins_amount_start, ins_amount_end + ins_amount_offset, ins_amount_offset)
+const query_matrix = R.pipe(
+  R.xprod(age_arr),
+  R.xprod(ins_amount_arr),
+  R.xprod(ins_pay_period),
+  R.map(R.flatten),
+  R.map(R.zipObj(['ins_pay_period', 'ins_amount', 'age', 'gender'])),
+)(gender)
+
+
+// console.log('helloworld'.bgWhite.black, query_matrix)
+let matrix_idx = 0
+let timer = setInterval(() => {
+  if (query_matrix[matrix_idx] === undefined) {
+    clearInterval(timer)
+  } else {
+    setData(query_matrix[matrix_idx++])
+  }
+}, timer_interval * 1000)
+function setData (dataset) {
+  const { age, gender, ins_amount, ins_pay_period} = dataset
+  console.log(`[age: ${age}, gender: ${gender}, ins_amount: ${ins_amount}, ins_pay_period: ${ins_pay_period === null ? '躉繳' : ins_pay_period + '年期'}] `.bgWhite.black)
+}
 const auth = new google.auth.JWT(
   credentials.client_email,
   null,
@@ -12,42 +93,54 @@ const auth = new google.auth.JWT(
 google.options({ auth })
 
 const sheets = google.sheets('v4')
-const spreadsheetId = '1EPz9qJO5RLYXhLiCZtVTdDVo-vLeozhGqmeR4lPc-aY'
+const spreadsheetId = '1yfwfh0StaFrvJuKyoJkMHojBFQcXAzcGXfys-LE-M70'
 
-sheets.spreadsheets.values.get({
-  spreadsheetId,
-  range: 'sheet1!ins_datas'
-}, (err, response) => {
-  console.log('==== initial values ====')
-  console.log(response.values)
-})
+// sheets.spreadsheets.values.get({
+//   spreadsheetId,
+//   range: 'sheet_input!ins_product_BYA'
+// }, (err, response) => {
+//   console.log('==== initial values ====')
+//   // console.log(response.values)
+//   const ins_product = _.map(response.values, (val) => {
+//     const ins_fee = parseInt(_.get(val, '[0]', 0).replace(/,/g, ''))
+//     const rate = _.get(val, '[1]', null)
+//     return ({ ins_fee, rate })
+//   })
+//   console.log(ins_product)
+//
+// })
 
-setTimeout(() => {
-  sheets.spreadsheets.values.update({
-    spreadsheetId,
-    range: 'sheet1!B2',
-    valueInputOption: 'USER_ENTERED',
-    includeValuesInResponse: true,
-    resource: {
-      values: [[Math.round(Math.random()*1000)]]
-    }
-
-  }, (err, response) => {
-    if (err) {
-      console.log('err', err)
-    } else {
-      console.log(`\nField ins_amount, ins_fee_calculated should update in 8 seconds. Check the difference. \n`)
-      setTimeout(() => {
-        sheets.spreadsheets.values.get({
-          spreadsheetId,
-          range: 'sheet1!ins_datas'
-        }, (err, response) => {
-          console.log('==== updated values ====')
-          console.log(response.values)
-        })
-      }, 8000)
-    }
-  })
-}, 2000)
+// setTimeout(() => {
+//   sheets.spreadsheets.values.update({
+//     spreadsheetId,
+//     range: 'sheet_input!ins_amount',
+//     valueInputOption: 'USER_ENTERED',
+//     includeValuesInResponse: true,
+//     resource: {
+//       values: [[ 10000 ]]
+//     }
+//
+//   }, (err, response) => {
+//     if (err) {
+//       console.log('err', err)
+//     } else {
+//       console.log(`\nField ins_amount, ins_fee_calculated should update in 8 seconds. Check the difference. \n`)
+//       setTimeout(() => {
+//         sheets.spreadsheets.values.get({
+//           spreadsheetId,
+//           range: 'sheet_input!ins_product_USL7'
+//         }, (err, response) => {
+//           console.log('==== updated values ====')
+//           const tmp = _.map(response.values, (val) => {
+//             const ins_fee = parseInt(_.get(val, '[0]', 0).replace(/,/g, ''))
+//             const rate = _.get(val, '[1]', null)
+//             return ({ ins_fee, rate })
+//           })
+//           console.log(tmp)
+//         })
+//       }, 30000)
+//     }
+//   })
+// }, 2000)
 
 
---
+++ models/product.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose'
+
+let ProductSchema = new mongoose.Schema({
+  age: String,
+  gender: String,
+  ins_amount: String,
+  ins_pay_period: String,
+  ins_fee: String,
+  ins_fee_with_discount: String,
+  forfeit_fee_matrix: String,
+  created_at: Date,
+  updated_at: Date
+})
+
+// use function insteat of => to bind this
+// ref: http://stackoverflow.com/questions/37365038/this-is-undefined-in-a-mongoose-pre-save-hook
+ProductSchema.pre('save', function (next) {
+  let st = this
+  if (!st.created_at) {
+    st.created_at = new Date()
+  } else {
+    st.updated_at = new Date()
+  }
+  next()
+})
+
+export default mongoose.model('Product', ProductSchema)
+
+
